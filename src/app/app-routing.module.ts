9af52b7a2989ab8f745369a449cb5992292638ec
NeranjanEkanayake <mrnb99@gmail.com>
2025-04-21 10:44:00 +0530

    Created Angular Application with Starting Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
+import { ViewBlogsComponent } from './components/view-blogs/view-blogs.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'blogs', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-blog', component: CreateBlogComponent },
+  { path: 'blogs', component: ViewBlogsComponent },
+  { path: 'blogs/:id', component: BlogDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
+import { ViewBlogsComponent } from './components/view-blogs/view-blogs.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateBlogComponent,
+    ViewBlogsComponent,
+    NavbarComponent,
+    BlogDetailsComponent
   ],
   imports: [
     BrowserModule,
